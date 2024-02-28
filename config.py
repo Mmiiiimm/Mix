55200ca98e69b5c819aae46a2c0d6e94888bcbaf
Mmiiiimm <129563335+Mmiiiimm@users.noreply.github.com>
2024-02-28 20:26:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -77,9 +77,9 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = "https://telegra.ph/file/1068724d97569bb1a4a35.jpg"
+START_IMG_URL = "https://i.postimg.cc/g2bq1SK1/IMG-3628.jpg"
 PING_IMG_URL = "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+PLAYLIST_IMG_URL = "https://i.postimg.cc/g2bq1SK1/IMG-3628.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
