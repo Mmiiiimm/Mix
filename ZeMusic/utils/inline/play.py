70bf581b740f87441508a1fc904aba7dd669bd49
Mmiiiimm <129563335+Mmiiiimm@users.noreply.github.com>
2024-02-28 21:42:45 +0200

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -57,10 +57,10 @@ def stream_markup_timer(_, chat_id, played, dur):
     buttons = [
                 [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
             InlineKeyboardButton(text=". aDD Me To Your Groups .", url=f"https://t.me/{app.username}?startgroup=true")],
     ]
@@ -71,10 +71,10 @@ def stream_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
             InlineKeyboardButton(text=". aDD Me To Your Groups .", url=f"https://t.me/{app.username}?startgroup=true")],
     ]
